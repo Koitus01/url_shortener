e6f48f35d5b860d2ba65f60a7089bc838c484621
Артём Быков <a.bykov@sprinthost.ru>
2023-04-17 19:57:12 +0300

    add composer install in entrypoint.sh and chown in php/Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,4 @@
 sleep 10
+docker exec -it url_shortener_app composer install
 docker exec -it url_shortener_app php bin/console doctrine:migrations:migrate -n
 docker exec -it url_shortener_app php bin/console --env=test doctrine:migrations:migrate -n
